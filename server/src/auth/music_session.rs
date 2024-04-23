fc4ac972471a46c3ec05a5b10acd2aad289af66e
mendess <pedro.mendes.26@gmail.com>
2024-04-23 16:51:23 +0100

    Avoid recreating token if it was already expired

--- server/src/auth/music_session.rs
+++ server/src/auth/music_session.rs
@@ -1,4 +1,9 @@
-use std::fmt;
+use std::{
+    fmt,
+    future::{ready, Future},
+    ops::ControlFlow,
+    str::FromStr,
+};
 
 use common::domain::Hostname;
 use serde::{Deserialize, Serialize};
@@ -14,7 +19,125 @@ impl fmt::Display for MusicSession {
     }
 }
 
+async fn handle_constraint_violations<'f, F, H, FFut, HFut, T>(
+    query: F,
+    try_handle_constraint: H,
+) -> sqlx::Result<T>
+where
+    F: Fn() -> FFut,
+    FFut: Future<Output = sqlx::Result<T>>,
+    H: Fn(Constraint) -> HFut,
+    HFut: Future<Output = sqlx::Result<ControlFlow<T>>>,
+{
+    loop {
+        let result = query().await;
+        match result {
+            Err(sqlx::Error::Database(error)) => {
+                match error.constraint().and_then(|c| c.parse().ok()) {
+                    Some(constraint) => {
+                        match try_handle_constraint(constraint).await? {
+                            ControlFlow::Continue(()) => continue,
+                            ControlFlow::Break(fallback) => return Ok(fallback),
+                        };
+                    }
+                    None => break Err(sqlx::Error::Database(error)),
+                }
+            }
+            result => break result,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Constraint {
+    UniqueId,
+    UniqueHostname,
+}
+
+impl FromStr for Constraint {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        const UNIQUE_ID_STR: &str = "music_session_unique_ids";
+        const UNIQUE_HOSTNAME_STR: &str = "music_sessions_unique_hostnames";
+        match s {
+            UNIQUE_ID_STR => Ok(Self::UniqueId),
+            UNIQUE_HOSTNAME_STR => Ok(Self::UniqueHostname),
+            _ => Err(()),
+        }
+    }
+}
+
 impl MusicSession {
+    pub async fn create_for_2(db: &PgPool, hostname: &Hostname) -> sqlx::Result<Self> {
+        let id = handle_constraint_violations(
+            || {
+                sqlx::query_scalar!(
+                    "INSERT INTO music_sessions (id, expires_at, hostname) VALUES
+                    (substr(md5(random()::text), 0, 7), NOW() + interval '4 hours', $1)
+                    RETURNING id",
+                    hostname.as_ref()
+                )
+                .fetch_one(db)
+            },
+            |constraint| async move {
+                match constraint {
+                    Constraint::UniqueId => Ok(ControlFlow::Continue(())),
+                    Constraint::UniqueHostname => update_existing_token(db, hostname).await,
+                }
+            },
+        )
+        .await?;
+
+        async fn update_existing_token(
+            db: &PgPool,
+            hostname: &Hostname,
+        ) -> sqlx::Result<ControlFlow<String>> {
+            let updated_id = sqlx::query_scalar!(
+                "UPDATE music_sessions
+                SET expires_at = (NOW() + interval '4 hours')
+                WHERE hostname = $1 AND expires_at > NOW()
+                RETURNING id",
+                hostname.as_ref()
+            )
+            .fetch_optional(db)
+            .await?;
+            Ok(ControlFlow::Break(match updated_id {
+                Some(id) => id,
+                // existing token has expired
+                None => overwrite_with_new_token(db, hostname).await?,
+            }))
+        }
+
+        async fn overwrite_with_new_token(
+            db: &PgPool,
+            hostname: &Hostname,
+        ) -> sqlx::Result<String> {
+            handle_constraint_violations(
+                || {
+                    sqlx::query_scalar!(
+                        "UPDATE music_sessions
+                        SET
+                            expires_at = NOW() + interval '4 hours',
+                            id = substr(md5(random()::text), 0, 7)
+                        WHERE hostname = $1
+                        RETURNING id",
+                        hostname.as_ref(),
+                    )
+                    .fetch_one(db)
+                },
+                |constraint| match constraint {
+                    Constraint::UniqueId => ready(Ok(ControlFlow::Continue(()))),
+                    Constraint::UniqueHostname => {
+                        unreachable!("not inserting a row with a hostname")
+                    }
+                },
+            )
+            .await
+        }
+
+        Ok(Self(id))
+    }
+
     pub async fn create_for(db: &PgPool, hostname: &Hostname) -> sqlx::Result<Self> {
         let id = loop {
             let result = sqlx::query!(
@@ -31,16 +154,45 @@ impl MusicSession {
                 Err(sqlx::Error::Database(error)) => match error.constraint() {
                     Some("music_session_unique_ids") => continue,
                     Some("music_sessions_unique_hostnames") => {
-                        break sqlx::query!(
+                        let updated_id = sqlx::query_scalar!(
                             "UPDATE music_sessions
                             SET expires_at = (NOW() + interval '4 hours')
-                            WHERE hostname = $1
+                            WHERE hostname = $1 AND expires_at > NOW()
                             RETURNING id",
                             hostname.as_ref()
                         )
-                        .fetch_one(db)
-                        .await?
-                        .id;
+                        .fetch_optional(db)
+                        .await?;
+                        match updated_id {
+                            Some(id) => {
+                                break id;
+                            }
+                            None => {
+                                break loop {
+                                    let result = sqlx::query_scalar!(
+                                        "UPDATE music_sessions
+                                        SET
+                                            expires_at = NOW() + interval '4 hours',
+                                            id = substr(md5(random()::text), 0, 7)
+                                        WHERE hostname = $1
+                                        RETURNING id",
+                                        hostname.as_ref(),
+                                    )
+                                    .fetch_one(db)
+                                    .await;
+                                    match result {
+                                        Ok(id) => break id,
+                                        Err(sqlx::Error::Database(error)) => {
+                                            match error.constraint() {
+                                                Some("music_session_unique_ids") => continue,
+                                                _ => return Err(sqlx::Error::Database(error)),
+                                            }
+                                        }
+                                        Err(e) => return Err(e),
+                                    }
+                                };
+                            }
+                        }
                     }
                     _ => return Err(sqlx::Error::Database(error)),
                 },
