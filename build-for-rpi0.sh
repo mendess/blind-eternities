99d37bb93fad6a8c5164a429852d382461119751
mendess <pedro.mendes.26@gmail.com>
2024-05-19 13:14:36 +0100

    add auto install of cargo-cross to build-for-rpi0.sh

--- build-for-rpi0.sh
+++ build-for-rpi0.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+set -e
+if ! hash cross; then
+    echo "Installing cargo cross..."
+    cargo install cross --git https://github.com/cross-rs/cross
+fi
+if ! pgrep docker >/dev/null; then
+    echo "Starting docker..."
+    sudo systemctl start docker
+fi
+
+target=goblinww
+case "$1" in
+    mirrodin)
+        target=mirrodin
+        ;;
+esac
+set -x
 cross build --target arm-unknown-linux-gnueabihf --bin spark --release
-spark rsync av ./target/arm-unknown-linux-gnueabihf/release/spark goblinww:
-spark ssh goblinww -- sudo install spark /usr/bin
+spark rsync av ./target/arm-unknown-linux-gnueabihf/release/spark $target:
+spark ssh $target -- sudo install spark /usr/bin
+spark ssh $target -- spark msg reload
